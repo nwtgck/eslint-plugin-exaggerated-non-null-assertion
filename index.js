6551836e0d2862bdc9dc91d98467f5417c1d1aca
Ryo Ota <nwtgck@nwtgck.org>
2020-06-07 15:45:09 +0900

    feat: support `eslint --fix`

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+// TODO: the number of "!" is hard coded
+
 module.exports = {
   rules: {
     "exaggerated-non-null-assertion": {
+      meta: {
+        messages: {
+          nonNullAssertionTooLess: 'Non-null assertion is too less'
+        },
+        fixable: 'code',
+      },
       create(context) {
         const visited = new WeakSet();
         return {
@@ -8,15 +16,24 @@ module.exports = {
             if (visited.has(node.parent)) {
               return;
             }
+            let nNonNullAssertion = 1;
             visited.add(node);
             if (node.expression !== undefined && node.expression.type === 'TSNonNullExpression') {
+              nNonNullAssertion++;
               visited.add(node.expression);
               if (node.expression.expression !== undefined && node.expression.expression.type === 'TSNonNullExpression') {
+                nNonNullAssertion++;
                 visited.add(node.expression.expression);
                 return;
               }
             }
-            context.report(node, 'Non-null assertion is too less');
+            context.report({
+              node,
+              messageId: 'nonNullAssertionTooLess',
+              fix(fixer) {
+                return fixer.insertTextAfter(node, "!".repeat(3 - nNonNullAssertion));
+              }
+            });
           },
         };
       }
