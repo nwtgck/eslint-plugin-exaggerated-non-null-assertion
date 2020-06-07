6df37c9d410c17cbdb4a98ae073422ebf14977f0
Ryo Ota <nwtgck@nwtgck.org>
2020-06-07 15:23:58 +0900

    refactor: correct local variable typo

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ module.exports = {
   rules: {
     "exaggerated-non-null-assertion": {
       create(context) {
-        const visted = new WeakSet();
+        const visited = new WeakSet();
         return {
           TSNonNullExpression(node) {
-            if (visted.has(node.parent)) {
+            if (visited.has(node.parent)) {
               return;
             }
-            visted.add(node);
+            visited.add(node);
             if (node.expression !== undefined && node.expression.type === 'TSNonNullExpression') {
-              visted.add(node.expression);
+              visited.add(node.expression);
               if (node.expression.expression !== undefined && node.expression.expression.type === 'TSNonNullExpression') {
-                visted.add(node.expression.expression);
+                visited.add(node.expression.expression);
                 return;
               }
             }
